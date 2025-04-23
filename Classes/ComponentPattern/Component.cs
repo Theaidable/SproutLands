95d6b05500bd0531abe5b552a8b42374e65e74fd
David Gudmund <dada5@student.eadania.dk>
2025-04-23 10:20:59 +0200

    Update Component.cs

--- Classes/ComponentPattern/Component.cs
+++ Classes/ComponentPattern/Component.cs
@@ -20,14 +20,13 @@ namespace SproutLands.Classes.ComponentPattern
             this.GameObject = gameObject;
         }
 
+        //Virtuelle metoder som skal bruges af components children
         public virtual void Awake() { }
-
         public virtual void Start() { }
         public virtual void Update() { }
-
-
         public virtual void Draw(SpriteBatch spriteBatch) { }
         public virtual void OnCollisionEnter(Collider collider) { }
+
 
         public void SetNewGameObject(GameObject gameObject)
         {
